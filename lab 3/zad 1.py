0916446aab7f65612b1b6576f0389e49201cfd2b
gierczak1235 <73538677+gierczak1235@users.noreply.github.com>
2021-03-13 09:24:21 +0100

    d

--- lab 3/zad 1.py
+++ lab 3/zad 1.py
@@ -1,3 +1,6 @@
 A=[1/x for x in range(1,10)]
-B=[i  for i in range(1,10) i=i*2]
+B=[2**i for i in range(int(0.5), 11)]
+C=[ i for i in B if i % 4 == 0]
+print(A)
 print(B)
+print(C)
---
+++ lab 3/zad 2 .py
@@ -0,0 +1,16 @@
+print("nie dziala bez numpy ;d ")
+import numpy as np
+import random
+count=0
+def incr():
+     global count
+     count += 1
+random.seed()
+A = np.zeros((4,4))
+for x in range(0,4):
+    for j in range(0,4):
+        d=random.randint(0,9)
+        A[x][j]=d
+print (':D', A)
+d=[incr() or row[x] for row in A for x in range(count,4,4)]
+print(d)
