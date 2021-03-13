5f39df9e5a21923d1c53a1f1d56637b8e6d33c50
gierczak1235 <73538677+gierczak1235@users.noreply.github.com>
2021-03-13 09:39:40 +0100

    :D

--- lab 3/zad 3.py
+++ lab 3/zad 3.py
@@ -1,9 +1,9 @@
-skroty = {"2zł": "Mleko",
-"3zł": "Ser",
-"PKO": "Powszechna Kasa Oszczędności"}
-odwrocone = {value: key for key, value in skroty.items()}
-
-print("Oryginalny słownik")
-print(skroty)
-print("Słownik odwrócony")
-print(odwrocone)
+xd = {"Mleko":"2zł",
+"Ser": "3zł",
+"soja": "200zł","złoto":"sztuki","diamenty":"sztuki"}
+#odwrocone = {value: key for key, value in xd.items()}
+g={ key for key , value in xd.items() if value=="sztuki" }
+print("sklep")
+print(xd)
+print("sztuki")
+print(g)
