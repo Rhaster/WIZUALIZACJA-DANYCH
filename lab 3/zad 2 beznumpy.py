afc3b166b21d12f9e32585e72a96f33e069f7211
gierczak1235 <73538677+gierczak1235@users.noreply.github.com>
2021-03-15 10:41:57 +0100

    xd

---
+++ lab 3/zad 2 beznumpy.py
@@ -0,0 +1,18 @@
+print("nie dziala bez numpy ;d ")
+import random
+count=0
+def incr():
+     global count
+     count += 1
+random.seed()
+A = [[0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]]
+for x in range(0,4):
+    for j in range(0,4):
+        d=random.randint(0,9)
+        A[x][j]=d
+print (':D', A)
+d=[incr() or row[x] for row in A for x in range(count,4,4)]
+print(d)
--- lab 3/zad 3.py
+++
@@ -1,9 +0,0 @@
-xd = {"Mleko":"2zł",
-"Ser": "3zł",
-"soja": "200zł","złoto":"sztuki","diamenty":"sztuki"}
-#odwrocone = {value: key for key, value in xd.items()}
-g={ key for key , value in xd.items() if value=="sztuki" }
-print("sklep")
-print(xd)
-print("sztuki")
-print(g)
